a31ae4b697f2d42ee7eb8adbb34221fe1ebde187
Andrew Makousky <mako0042@umn.edu>
2011-07-20 20:56:54 -0500

    Save file.

---
+++ HexEdit.h
@@ -0,0 +1,9 @@
+//Defines general layout for reworked text editor
+
+#ifndef TEXTEDIT_H
+#define TEXTEDIT_H
+
+//Public function delcarations
+LRESULT CALLBACK TextEditProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+#endif
