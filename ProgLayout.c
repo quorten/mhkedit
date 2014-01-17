5d8542d22657b361882ec8cd8ff7251835d4120c
Andrew Makousky <mako0042@umn.edu>
2014-01-17 00:14:10 -0600

    Save file.

---
+++ ProgLayout.c
@@ -0,0 +1,8 @@
+/* A convenience library for maintaing fixed, programmer-defined
+   layouts.
+
+   This library is useful for simple applications that may have a
+   tool-bar, a status bar, and a few side panes that really don't need
+   full drag-and-drop customizability like that which is tool band.
+
+*/
